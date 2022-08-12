a00f0fc899d2560425848d356b13431fc40fe4ef
GomezFrannco <frangomez.baires@gmail.com>
2022-08-12 19:52:20 -0300

    [Add] User DAO class

---
+++ src/services/users/dao.users.js
@@ -0,0 +1,23 @@
+const UserModel = require("../../models/user.models");
+
+class MongoUserDAO { // Users CRUD with MongoDB
+  constructor() {
+    this.model = UserModel; // Mongoose model
+  }
+  async createUser(input) {
+    return await this.model.create(input);
+  }
+  async getAllUsers() {
+    const users = await this.model.findOne();
+    return users; 
+  }
+  async getUser(id) {
+    const user = await this.model.findById(id);
+    return user;
+  }
+  async getUserByEmail(email) {
+    return await this.model.findOne({ email })
+  }
+}
+
+module.exports = MongoUserDAO;
