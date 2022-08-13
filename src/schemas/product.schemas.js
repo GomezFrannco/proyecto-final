5d7177ece7365153c45ba8cbf4a192b739ba1794
GomezFrannco <frangomez.baires@gmail.com>
2022-08-13 19:57:11 -0300

    [Add] yup create product schema

---
+++ src/schemas/product.schemas.js
@@ -0,0 +1,13 @@
+const { object, string, number } = require("yup");
+
+const createProductSchema = object({
+  productName: string().required("Product name is required"),
+  category: string().required("Category is required"),
+  description: string().min(10).max(180).default("This product doesn't have any description"),
+  price: number().positive("It can't be a negative number").required("Price is required"),
+  stock: number().positive("It can't be a negative number").required("Stock is required"),
+});
+
+module.exports = {
+  createProductSchema,
+};
