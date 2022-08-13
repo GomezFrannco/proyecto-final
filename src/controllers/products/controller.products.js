0addb208c5ce250c395c90b830cdcf157b3f203d
GomezFrannco <frangomez.baires@gmail.com>
2022-08-13 20:01:03 -0300

    [Add] Products Gett handlers has been created

--- src/controllers/products/controller.products.js
+++ src/controllers/products/controller.products.js
@@ -1,5 +1,6 @@
-const { createProductSchema } = require("../../schemas/product.schemas");
+const { createProductSchema, updateProductSchema } = require("../../schemas/product.schemas");
 const MongoProductDAO = require("../../services/products/dao.products");
+const ProductDTO = require("../../services/products/dto.products");
 const { log } = require("../../utils/log4js.utils");
 
 class ProductPostHandlers {
@@ -31,8 +32,56 @@ class ProductPostHandlers {
   }
 }
 
+class ProductGetHandlers {
+  async getProductByIdHandler(req, res) {
+    const { id } = req.params;
+    try {
+      const product  = await new MongoProductDAO().getProductById(id);
+      const dto = new ProductDTO();
+      dto.setPicture(product.tumbnail);
+      dto.setName(product.productName);
+      dto.setDescription(product.description);
+      dto.setPrice(product.price);
+      dto.setStock(product.stock);
+      return res.status(200).json({
+        Response: {
+          Product: dto,
+        },
+      });
+    } catch (error) {
+      log.file.error(error.message);
+      log.console.debug(error.message);
+      return res.status(500).json({
+        Error: {
+          Message: error.message,
+        },
+      });
+    }
+  }
+  async getAllProductsHandler(_req, res) {
+    try {
+      const products = await new MongoProductDAO().getAllProducts();
+      // acá hay que retornar un DTO
+      return res.status(200).json({
+        Response: {
+          Products: products,
+        },
+      });
+    } catch (error) {
+      log.file.error(error.message);
+      log.console.debug(error.message);
+      return res.status(500).json({
+        Error: {
+          Message: error.message,
+        },
+      });
+    }
+  }
+}
+
 module.exports = {
   handlers: {
     post: new ProductPostHandlers(),
+    get: new ProductGetHandlers(),
   },
 };
