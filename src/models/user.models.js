5d41b8014f3c13ced8060dc8e6f1bcd06f28808b
GomezFrannco <frangomez.baires@gmail.com>
2022-08-13 19:51:59 -0300

    [Updt] password encrypt

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,19 +1,23 @@
-const { Schema, model, project } = require("mongoose");
+const { Schema, model } = require("mongoose");
+const argon2 = require("argon2");
 
 const userModel = new Schema(
   {
     email: {
       type: String,
+      trim: true,
       lowercase: true,
       required: true,
       unique: true,
     },
     firstName: {
       type: String,
+      trim: true,
       required: true,
     },
     lastName: {
       type: String,
+      trim: true,
       required: true,
     },
     password: {
@@ -26,6 +30,18 @@ const userModel = new Schema(
     timestamps: true,
   }
 );
+
+// Encriptando la contraseña del usuario
+// cada vez que se guarda un documento.
+userModel.pre("save", async function () {
+  // Se verifica que la contraseña no se haya modificado.
+  if (!this.isModified("password")) {
+    return;
+  }
+  const hash = await argon2.hash(this.password);
+  this.password = hash;
+  return;
+});
 
 const UserModel = new model("Users", userModel);
 
