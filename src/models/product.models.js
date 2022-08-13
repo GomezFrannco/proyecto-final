ef182df1847309e0b48109d5fe82fceb2367832b
GomezFrannco <frangomez.baires@gmail.com>
2022-08-13 19:54:26 -0300

    [Add] Product mongoose schema created

---
+++ src/models/product.models.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+const { nanoid } = require("nanoid");
+
+const productModel = new Schema(
+  {
+    productName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    productCode: {
+      type: String,
+      unique: true,
+      default: () => nanoid(6),
+    },
+    thumbnail: {
+      type: String,
+      trim: true,
+      required: true,
+      default: "uploads/default-image.png",
+    },
+    category: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: "This product doesn't have any description",
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProductModel = new model("Products", productModel);
+
+module.exports = ProductModel;
