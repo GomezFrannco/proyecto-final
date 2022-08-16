a1afb63da6230f3002254d893acd154515379112
GomezFrannco <frangomez.baires@gmail.com>
2022-08-16 17:25:24 -0300

    [Updt] yup middleware added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const { handlers } = require("../controllers/users/controller.users");
+const validateResource = require("../middlewares/validations.middlewares");
+const { createUserSchema } = require("../schemas/user.schemas");
 
 const router = Router();
 
-router.post("/api/users", handlers.post.createUserHandler);
+router.post("/api/users", validateResource(createUserSchema), handlers.post.createUserHandler);
 
 module.exports = router;
