a322c05b684056b551931f1f360b619ab74ce662
GomezFrannco <frangomez.baires@gmail.com>
2022-08-16 17:25:01 -0300

    [Updt] yup middleware added

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,14 +1,16 @@
 const { Router } = require("express");
 const { handlers } = require("../controllers/products/controller.products");
+const validateResource = require("../middlewares/validations.middlewares");
+const { createProductSchema, updateProductSchema } = require("../schemas/product.schemas");
 
 const router = Router();
 
 router.route("/api/products")
   .get(handlers.get.getAllProductsHandler)
-  .post(handlers.post.createProductHandler)
+  .post(validateResource(createProductSchema) ,handlers.post.createProductHandler)
 router.route("/api/products/:id")
   .get(handlers.get.getProductByIdHandler)
-  .put(handlers.put.updateProductHandler)
+  .put(validateResource(updateProductSchema), handlers.put.updateProductHandler)
   .delete(handlers.delete.deleteProductHandler)
 
 module.exports = router;
