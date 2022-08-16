c8421533a98144b6ec87d4328c42dbdd3aa1a2d4
GomezFrannco <frangomez.baires@gmail.com>
2022-08-16 17:22:23 -0300

    [Add] yup validations middleware created

---
+++ src/middlewares/validations.middlewares.js
@@ -0,0 +1,19 @@
+const validateResource = (schema) => async (req, res, next) => {
+  const resource = req.body;
+  try {
+    await schema.validate(resource, {
+      strict: true,
+      abortEarly: true,
+    });
+    next();
+  } catch (error) {
+    res.status(400).json({
+      Error: {
+        Path: error?.path,
+        Message: error?.message, 
+      }
+    })
+  }
+};
+
+module.exports = validateResource;
