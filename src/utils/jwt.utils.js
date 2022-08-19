9fa12ea77574e6401e61196413ae06e2ee57adf0
GomezFrannco <frangomez.baires@gmail.com>
2022-08-19 19:56:32 -0300

    [Updt] comments has been added

--- src/utils/jwt.utils.js
+++ src/utils/jwt.utils.js
@@ -1,17 +1,21 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-function signJwt(payload, keyName) {
+function signJwt(payload, keyName, options) {
+  // Creamos un buffer para usarlo como secret
   const signingKey = Buffer.from(config.get(keyName), "base64").toString("ascii");
+  // Registramos o creamos el token
   return jwt.sign(payload, signingKey, {
-    ...(options && options),
+    ...(options && options), // Las opciones de los parametros.
     algorithm: "RS256",
   });
 }
 
 function verifyJwt(token, keyName) {
-  const publicKey = Buffer.from(config.get(keyName), "base64").toString("ascii");
+  // Creamos un buffer para usarlo como secret.
+  const publicKey = Buffer.from(config.get(keyName) , "base64").toString("ascii");
   try {
+    // Verificamos que el token recibido sea correcto.
     const decoded = jwt.verify(token, publicKey);
     return decoded;
   } catch (error) {
