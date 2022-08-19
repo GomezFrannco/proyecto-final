872cfe56d9106ddfeb5d4607d2ca90e249fdaa7f
GomezFrannco <frangomez.baires@gmail.com>
2022-08-19 19:57:48 -0300

    [Updt] comments has been added

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -6,11 +6,16 @@ const { createProductSchema, updateProductSchema } = require("../schemas/product
 const router = Router();
 
 router.route("/api/products")
+  // visualizamos todos los productos disponibles en la base de datos
   .get(handlers.get.getAllProductsHandler)
+  // agregamos productos a la base de datos
   .post(validateResource(createProductSchema) ,handlers.post.createProductHandler)
 router.route("/api/products/:id")
+  // visualizamos productos individualmente por su id
   .get(handlers.get.getProductByIdHandler)
+  // actualizamos productos por su id
   .put(validateResource(updateProductSchema), handlers.put.updateProductHandler)
+  // borramos productos por su id
   .delete(handlers.delete.deleteProductHandler)
 
 module.exports = router;
