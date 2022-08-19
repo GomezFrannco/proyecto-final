9ae837c22eb9a7da06eb1ff9f590deffd3729ad4
GomezFrannco <frangomez.baires@gmail.com>
2022-08-19 20:06:40 -0300

    [Updt] Get user endpoint has been created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,19 @@
 const { Router } = require("express");
 const { handlers } = require("../controllers/users/controller.users");
 const validateResource = require("../middlewares/validations.middlewares");
+const requireUser = require("../middlewares/requireUser.middlewares")
 const { createUserSchema } = require("../schemas/user.schemas");
 
 const router = Router();
 
 router.post("/api/users", validateResource(createUserSchema), handlers.post.createUserHandler);
+
+// router.post("/api/user/verify/:id/:verificationCode", validateResource(verifyUserSchema), handlers.post.verifyUserHandler);
+
+// router.post("/api/users/forgotpassword", validateResource(forgotPasswordSchema), handlers.post.forgotPasswordHandler);
+
+// router.post("api/user/resetpassword/:id/:passwordResetCode", validateResource(resetPasswordSchema), handlers.post.resetPasswordHandler);
+
+router.get("/api/users/me", requireUser, handlers.get.getCurrentUserHandler);
 
 module.exports = router;
