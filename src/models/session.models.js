4071e7d8a536380989af23e95e0f111039ec9d0d
GomezFrannco <frangomez.baires@gmail.com>
2022-08-19 20:00:48 -0300

    [Add] Session mongoose model has been created

---
+++ src/models/session.models.js
@@ -0,0 +1,22 @@
+const { Schema, model, SchemaType} = require("mongoose");
+const { userModel } = require("./user.models");
+
+const sessionModel = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: () => userModel,
+    },
+    valid: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const SessionModel = new model("Sessions", sessionModel);
+
+module.exports = SessionModel;
