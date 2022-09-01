7c4c724701959f656d99bed473a256b6e6229550
GomezFrannco <frangomez.baires@gmail.com>
2022-09-01 00:58:57 -0300

    [Updt] middleware response was modified

--- src/middlewares/requireUser.middlewares.js
+++ src/middlewares/requireUser.middlewares.js
@@ -2,7 +2,7 @@ const requireUser = (_req, res, next) => {
   // Utilizamos una variable almacenada localmente
   const user = res.locals.user;
   if (!user) {
-    return res.sendStatus(403);
+    return res.status(401).redirect("/login");
   }
   return next();
 };
