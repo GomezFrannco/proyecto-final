523b9e031b1fefde5fb5468b861c3455a9a85f5a
GomezFrannco <frangomez.baires@gmail.com>
2022-12-27 20:01:43 -0300

    Change endpoints handlers

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { handlers } = require("../controllers/products/controller.products");
+const { get, post, put, del } = require("../controllers/products/controller.products");
 const validateResource = require("../middlewares/validations.middlewares");
 const { createProductSchema, updateProductSchema } = require("../schemas/product.schemas");
 
@@ -7,15 +7,15 @@ const router = Router();
 
 router.route("/api/products")
   // visualizamos todos los productos disponibles en la base de datos
-  .get(handlers.get.getAllProductsHandler)
+  .get(get.GetAllProductsHandler)
   // agregamos productos a la base de datos
-  .post(validateResource(createProductSchema) ,handlers.post.createProductHandler)
+  .post(validateResource(createProductSchema), post.CreateProductHandler)
 router.route("/api/products/:id")
   // visualizamos productos individualmente por su id
-  .get(handlers.get.getProductByIdHandler)
+  .get(get.GetProductByIdHandler)
   // actualizamos productos por su id
-  .put(validateResource(updateProductSchema), handlers.put.updateProductHandler)
+  .put(validateResource(updateProductSchema), put.UpdateProductHandler)
   // borramos productos por su id
-  .delete(handlers.delete.deleteProductHandler)
+  .delete(del.DeleteProductHandler)
 
 module.exports = router;
