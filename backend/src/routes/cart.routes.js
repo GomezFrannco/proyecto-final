337e45b282cfa6b01e3412e6ee626ff9c2da7499
GomezFrannco <frangomez.baires@gmail.com>
2022-12-28 01:56:30 -0300

    Change methods

--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -1,13 +1,11 @@
 const { Router } = require("express");
-const { get, put } = require("../controllers/cart/controller.cart");
+const { get, post, put } = require("../controllers/cart/controller.cart");
 
 const router = Router();
 
-// router.post("/api/carts")
-//   .post(handlers.post.createCartHandler)
-
 router.route("/api/carts/:id")
-  .get(get.getCartByIdHandler)
-  .put(put.updateCartByIdHandler)
+  .get(get.GetCartByIdHandler)
+  .post(post.CreateOrUpdateCartHandler)
+  .put(put.UpdateCartByIdHandler)
 
 module.exports = router;
