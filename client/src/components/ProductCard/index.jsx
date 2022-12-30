266f636d243c2a32221cf91de643a7d97af732dd
GomezFrannco <frangomez.baires@gmail.com>
2022-12-30 18:29:39 -0300

    Change some styles

--- client/src/components/ProductCard/index.jsx
+++ client/src/components/ProductCard/index.jsx
@@ -4,13 +4,13 @@ function ProductCard({ name, price, category, imageUrl }) {
   return (
     <div className='product__card bg-white w-[19%] rounded-xl'>
       <img className="rounded-t-xl" src={imageUrl} alt={name} />
-      <div className='px-6 py-4 border-t-gray-200 border-t-[1px]'>
+      <div className='card__info px-6 py-4 border-t-gray-200 border-t-[1px]'>
         <div>
-          <p className="font-bold text-xl">
+          <p className="info__price font-bold text-2xl text-gray-800">
             $ {price}
           </p>
-          <p className="text-sm font-normal text-gray-500">{category}</p>
-          <p className="text-lg">{name}</p>
+          <p className="info__category text-sm font-normal text-gray-500">{category}</p>
+          <p className="info__name text-base font-bold">{name}</p>
         </div>
 
       </div>
